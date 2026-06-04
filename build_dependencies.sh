8b3fba5cb8b306306097c1f507ecdca111072afb
rajaLucy <rajakiranyadavalli@gmail.com>
2026-06-04 17:26:24 +0530

    Update testframework to 1.0.12 and fix entservices-helpers build

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -25,7 +25,7 @@ git clone --branch R4.4.1 https://github.com/rdkcentral/Thunder.git
 git clone --branch main https://github.com/rdkcentral/entservices-apis.git
 
 cd ..
-git clone --branch feature/RDKEMW-16743 https://github.com/rdkcentral/entservices-helpers.git
+git clone --branch develop https://github.com/rdkcentral/entservices-helpers.git
 cd "$GITHUB_WORKSPACE"
 
 # Safely clone entservices-testframework without exposing GITHUB_TOKEN in logs
@@ -34,7 +34,7 @@ case $- in
     *x*) xtrace_was_set=true ;;
 esac
 set +x
-git -c http.extraheader="Authorization: Bearer ${GITHUB_TOKEN}" clone https://github.com/rdkcentral/entservices-testframework.git
+git -c http.extraheader="Authorization: Bearer ${GITHUB_TOKEN}" clone --branch 1.0.12 https://github.com/rdkcentral/entservices-testframework.git
 if [ "$xtrace_was_set" = true ]; then
     set -x
 fi
@@ -99,6 +99,21 @@ cmake -G Ninja -S entservices-apis  -B build/entservices-apis \
 cmake --build build/entservices-apis --target install
 
 
+############################
+# generating minimal mock headers
+cd $GITHUB_WORKSPACE/entservices-testframework/Tests
+mkdir -p headers
+cd headers
+touch secure_wrapper.h
+touch wpa_ctrl.h
+touch rdk_logger_milestone.h
+mkdir -p rdk/iarmbus
+touch rdk/iarmbus/libIARM.h
+touch rdk/iarmbus/libIBus.h
+touch iarm.h
+cd $GITHUB_WORKSPACE
+
+
 #############################
 # Build entservices-helpers
 echo "======================================================================================"
@@ -106,7 +121,8 @@ echo "building entservices-helpers"
 cmake -G Ninja -S ../entservices-helpers -B build/entservices-helpers \
     -DEXCEPTIONS_ENABLE=ON \
     -DCMAKE_INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr" \
-    -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake"
+    -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
+    "-DCMAKE_CXX_FLAGS=-I$GITHUB_WORKSPACE/entservices-testframework/Tests/mocks -I$GITHUB_WORKSPACE/entservices-testframework/Tests/headers -I$GITHUB_WORKSPACE/entservices-testframework/Tests/headers/rdk/iarmbus -include $GITHUB_WORKSPACE/entservices-testframework/Tests/mocks/Iarm.h "
 cmake --build build/entservices-helpers --target install
 
 

eb5e804d3440e6dff7701f982e14d28712f877d2
Sergey Borushevsky <Sergey_Borushevsky2@cable.comcast.com>
2026-06-09 20:40:45 +0300

    Update to use common helpers. Got rid of patches for testframework.

--- cov_build.sh
+++ cov_build.sh
@@ -29,6 +29,7 @@ cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-backupmanager \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/rdk/halif/deepsleep-manager \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/thunder \
+-I ${GITHUB_WORKSPACE}/install/usr/include/wpeframework/helpers \
 -Wall -Werror -Wno-error=format \
 -Wl,-wrap,system -Wl,-wrap,popen -Wl,-wrap,syslog \
 -DDISABLE_SECURITY_TOKEN -DUSE_THUNDER_R4=ON -DTHUNDER_VERSION=4 -DTHUNDER_VERSION_MAJOR=4 -DTHUNDER_VERSION_MINOR=4" \
